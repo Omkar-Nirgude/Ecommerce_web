f58280bb6cb1dff613a0daeb37d728c02c57db40
Omkar-Nirgude <omkarnirgude25@gmail.com>
2026-08-09 20:03:26 +0530

    Today work is over

--- Ecommerce_web/src/components/Navbar/Navbar.jsx
+++ Ecommerce_web/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,66 @@
 import React from 'react'
 import Logo from "../../assets/Logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from 'react-icons/fa6';
+import { FaCartShopping, FaCaretDown } from "react-icons/fa6";
 import  DarkMode  from './DarkMode' ;
+
+
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
+
 
 const Navbar = () => {
   return (
+    <>
     <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='bg-primary/40 py-2'>
             <div className='container flex justify-between items-center'>
                 <div className=''>
-                    <a href="#" className='font-bold text-2xl sm:3xl flex gap-2'>
+                    <a href="#" className='font-bold text-2xl sm:text-3xl flex gap-2'>
                         <img src={Logo} alt="Logo" className='w-10'/>NexCart
                     </a>
                 </div>
@@ -39,6 +89,46 @@ const Navbar = () => {
             </div>
         </div>
     </div>
+
+
+    <div data-aos="zoom-in" className='flex justify-center'>
+        <ul className='sm:flex hidden items-center gap-4'>
+            {Menu.map((data) =>(
+                <li key={data.id}>
+                    <a href = {data.link}
+                    className='inline-block px-4 hover:text-primary duration-200'
+                    >
+                        {data.name}
+                    </a>
+                </li>
+            ))}
+            <li className='group relative cursor-pointer'>
+                <a href="#" className='flex items-center gap-2px py-2'>
+                    Trending Products 
+                    <span>
+                        <FaCaretDown className='transition-all duration-200 group-hover:rotate-180 ' />
+                    </span>
+                </a>
+                <div className=' absolute z-(9999) hidden group-hover:block w-200px rounded-md bg-white p-2 text-black shadow-md'>
+                    <ul>
+                        {DropdownLinks.map((data) =>(
+                            <li key={data.id}>
+                                <a href={data.link}
+                                className='inline-block w-full rounded-md p-2 hover:bg-primary/20'
+                                >
+
+                                    {data.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            
+            </li>
+        </ul>
+    </div>
+
+</>
   )
 }
 
