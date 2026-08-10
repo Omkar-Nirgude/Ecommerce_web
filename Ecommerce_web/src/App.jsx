51454c9902c23b3a332c3b1d088cf5ca35506b14
Omkar-Nirgude <omkarnirgude25@gmail.com>
2026-08-10 23:59:18 +0530

    over

--- Ecommerce_web/src/App.jsx
+++ Ecommerce_web/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Product from './components/Product/Product'
+import TopProduct from './components/TopProduct/TopProduct'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Product/>
+      <TopProduct/>
     </div>
   )
 }
